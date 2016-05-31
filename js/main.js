e38c253618daa0d69e2b34483404f182d27e0e68
Alex Efremov <alex.efremov.dev@gmail.com>
2016-05-31 12:26:18 +0300

    Change API to Wisdom API

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("#getQuote").on("click", function() {
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
-      $(".quote").text(json[0].content);
-      $(".author").text(json[0].title);
+    $.getJSON("https://wisdomapi.herokuapp.com/v1/random?callback=?", function(json) {
+      $(".quote").html(json.content);
+      $(".author").text(json.author.name);
     });
   });
 });
