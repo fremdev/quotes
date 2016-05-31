99412ab17015bcbb24486af3ee63efbbd02953bc
Alex Efremov <alex.efremov.dev@gmail.com>
2016-05-31 17:56:03 +0300

    Styling page, add tweetThis button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
-  $("#getQuote").on("click", function() {
+
+  function getQuote() {
     $.getJSON("https://wisdomapi.herokuapp.com/v1/random?callback=?", function(json) {
       $(".quote").html(json.content);
       $(".author").text(json.author.name);
     });
-  });
+  }
+  function tweetThis(){
+    window.open('http://www.google.com/','_blank');
+     
+   }
+
+  $("#tweetThis").on('click', tweetThis);
+
+  getQuote();
+  $("#nextQuote").on("click", getQuote);
 });
